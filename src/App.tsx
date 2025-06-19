3548e4db67722742d5e514e088bc9c1b7adfce0a
Vasjukvladimir <egorwasjuk@mail.ru>
2025-06-19 08:04:47 +0300

    Delete src directory

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { RouterProvider } from '@tanstack/react-router';
-import { router } from './router/routes';
-
-const App = () => {
-  return <RouterProvider router={router} />;
-};
-
-export default App;
